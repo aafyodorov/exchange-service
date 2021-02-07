043cd639af5b5758ad2f8321228b2451fd6a4ef0
fgavin <fgavin@student.21-school.ru>
2021-02-07 20:10:01 +0300

    change return type MainController.getGif() to ResponseEntity

--- src/main/java/tk/exchangeservice/controllers/MainController.java
+++ src/main/java/tk/exchangeservice/controllers/MainController.java
@@ -4,15 +4,13 @@ import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
 import tk.exchangeservice.clients.GiphyClient;
 import tk.exchangeservice.clients.OpenExchangeRatesClient;
-import tk.exchangeservice.dto.ModelAndViewErrorMessageBuilder;
 import tk.exchangeservice.model.GiphyService;
 import tk.exchangeservice.model.OpenExchangeRatesService;
 
@@ -27,16 +25,10 @@ import java.net.URI;
 public class MainController {
 	private OpenExchangeRatesService ratesService;
 	private GiphyService giphyService;
-	private ModelAndViewErrorMessageBuilder messageBuilder;
 	@Value("${giphy.broke}")
 	private String BROKE;
 	@Value("${giphy.rich}")
 	private String RICH;
-	@Autowired
-	@Qualifier(value = "builder")
-	public void setMessageBuilder(ModelAndViewErrorMessageBuilder messageBuilder) {
-		this.messageBuilder = messageBuilder;
-	}
 
 	@Autowired
 	public void setRatesService(OpenExchangeRatesService ratesService) {
@@ -48,15 +40,13 @@ public class MainController {
 		this.giphyService = giphyService;
 	}
 
-	public GiphyService getGiphyService() {
-		return giphyService;
-	}
-
 	@GetMapping("/gif")
-	public ModelAndView getGif(@RequestParam(name = "cur", required = false) String currency) {
+	public ResponseEntity<String> getGif(@RequestParam(name = "cur", required = false) String currency) {
 		if (currency == null) {
-			return messageBuilder.setStatus(400).setMessage("no_cur_parameter")
-				.setDescription("Required parameter 'cur' is not present. Add").build();
+			return ResponseEntity
+				.status(400)
+				.header("Content-Type", "application/json")
+				.body("{\"message\" : \"Required parameter 'cur' is not present. Add\"}");
 		}
 		try {
 			String gifTag;
@@ -66,23 +56,37 @@ public class MainController {
 				gifTag = BROKE;
 			}
 			URI gifUri = giphyService.getGif(gifTag);
-			return new ModelAndView("redirect:" + gifUri.toString());
+			return ResponseEntity
+				.status(303)
+				.header("Location", gifUri.toString())
+				.body("");
 		} catch (IllegalArgumentException ex) {
-			messageBuilder.setStatus(400).setMessage("invalid_currency_code")
-				.setDescription("The currency code must be three letters long. The list of currencies is available here:" +
-					" https://docs.openexchangerates.org/docs/supported-currencies");
-			return messageBuilder.build();
+			return ResponseEntity
+				.status(400)
+				.header("Content-Type", "application/json")
+				.body("{\"message\" : \"The currency code must be three letters long. The list of currencies is " +
+					"available here: https://docs.openexchangerates.org/docs/supported-currencies\"}");
 		} catch (Exception ex) {
+			int status;
+			String message;
+			JSONObject body = new JSONObject();
 			JSONObject jsonObject = new JSONObject(ex.getCause().getMessage());
+
 			String causedClassName = jsonObject.getJSONArray("source").get(0).toString().split("#")[0];
-			messageBuilder.setMessage(jsonObject.getString("message"));
 			if (causedClassName.contains(OpenExchangeRatesClient.class.getSimpleName())) {
-				messageBuilder.setStatus(jsonObject.getInt("status")).setDescription(jsonObject.getString(
-					"description"));
+				message = jsonObject.getString("description");
+				status = jsonObject.getInt("status");
 			} else if (causedClassName.contains(GiphyClient.class.getSimpleName())) {
-				messageBuilder.setStatus(400).setDescription(jsonObject.getString("message"));
+				message = jsonObject.getString("message");
+				status = 401;
+			} else {
+				throw new UnknownError();
 			}
-			return messageBuilder.build();
+			body.put("message", message);
+			return ResponseEntity
+				.status(status)
+				.header("Content-Type", "application/json")
+				.body(body.toString());
 		}
 	}
 }
--- src/test/java/tk/exchangeservice/controllers/MainControllerTestMocked.java
+++ src/test/java/tk/exchangeservice/controllers/MainControllerTestMocked.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -22,6 +23,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @RunWith(SpringRunner.class)
 @AutoConfigureMockMvc
+//@WebMvcTest(MainController.class)
 public class MainControllerTestMocked {
 
 	@Autowired
@@ -36,11 +38,11 @@ public class MainControllerTestMocked {
 	@MockBean
 	GiphyService giphyServiceMocked;
 
-	@Test(expected = IllegalArgumentException.class)
+	@Test//(expected = IllegalArgumentException.class)
 	public void getGif_getGifThrowIllegalArgumentException() throws Exception {
 		when(giphyServiceMocked.getGif(anyString())).thenThrow(IllegalArgumentException.class);
 		//controller.getGif(anyString());
 		mockMvc.perform(get("/gif?cur=RUB"));
-		//mockMvc.perform(get("/gif?cur=RUB")).andDo(print()).andExpect(status().is(400));
+		mockMvc.perform(get("/gif?cur=RUB")).andDo(print()).andExpect(status().is(400));
 	}
 }

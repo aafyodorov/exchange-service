ddb73fb67482ad69675c9ee48dc2aa4506f984a7
fgavin <andreyfedorov43@gmail.com>
2021-02-24 04:42:18 +0300

    add 2nd logger

--- autobuild/push-app.sh
+++ autobuild/push-app.sh
@@ -9,7 +9,6 @@ mv /home/travis/build/aafyodorov/exchange-service/target/*.jar ./autobuild/
 git config --global user.email "travis@travis-ci.com"
 git config --global user.name "Travis CI"
 git remote set-url origin "https://aafyodorov:${GIT_HUB_ACCESS_TOKEN}@github.com/aafyodorov/exchange-service"
-# /dev/null 2>&1
 
 git rm --cached ./autobuild/*.jar
 git add ./autobuild/*.jar
